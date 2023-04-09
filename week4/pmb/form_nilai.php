a597b585b3f2512600a964d2fb675bb392c460d3
salshabilah784 <114566749+salshabilah784@users.noreply.github.com>
2023-04-09 13:54:08 +0700

    Update form_nilai.php

--- week4/pmb/form_nilai.php
+++ week4/pmb/form_nilai.php
@@ -1,24 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<body>
+<!-- panggil file navbar -->
+<!-- panggil file sidebar -->
 <?php
 include_once('navbar.php');
 include_once('sidebar.php');
 ?>
+<!-- isi content -->
+<!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <div class="content-header">
       <div class="container-fluid">
-        <div class="row">
-          <div class="col-sm-12">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Praktikum 2</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
               <li class="breadcrumb-item active">Dashboard v1</li>
@@ -27,6 +23,22 @@ include_once('sidebar.php');
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrasi IT Club</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
     <h2 style="text-align: center;">Form Nilai Mahasiswa</h2>
     <hr/>
     <div class="container">
