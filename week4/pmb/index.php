4165bcace4f6ae49e7d9bfcc6b45bc7dac977e01
salshabilah784 <114566749+salshabilah784@users.noreply.github.com>
2023-04-09 13:34:59 +0700

    Update index.php

--- week4/pmb/index.php
+++ week4/pmb/index.php
@@ -1,7 +1,10 @@
+<?php
+require_once 'auth.php';
+?>
 <!doctype html>
 <html lang="en">
   <head>
-  	<title>login</title>
+  	<title>Login</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -23,17 +26,17 @@
 			<div class="row justify-content-center">
 				<div class="col-md-6 col-lg-4">
 					<div class="login-wrap p-0">
-		      	<h3 class="mb-4 text-center">Have an account?</h3>
-		      	<form method="POST" action="index.php" class="signin-form">
+		      	<form method="POST" action="home.php" class="signin-form">
+				  <?php if(isset($error)) { echo '<h5 style="color:yellow;">' . $error . '<h5/>'; } ?>
 		      		<div class="form-group">
 		      			<input type="text" id="username" name="username" class="form-control" placeholder="Username" required>
 		      		</div>
 	            <div class="form-group">
-	              <input id="password-field" name="password" type="password" class="form-control" placeholder="Password" required>
+	              <input id="password" name="password" type="password" class="form-control" placeholder="Password" required>
 	              <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
 	            </div>
 	            <div class="form-group">
-	            	<input type="submit" name="login" value="login" class="form-control btn btn-primary submit px-3">
+                <input type="submit" name="login" value="Login" class="form-control btn btn-primary submit px-3">
 	            </div>
 	            <div class="form-group d-md-flex">
 	            	<div class="w-50">
@@ -60,29 +63,3 @@
 
 	</body>
 </html>
-<?php
-session_start();
-// cek dulu apakah user sudah submit form login apa belum
-if (isset($_POST['login'])){
-// ambil data dari form login yang disubmit oleh user
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-
-// validasi username dan password yang di submit oleh user
-// lakukan perbandian dengan nilai hard-code atau username dan password nya
-if ($username == 'admin' && $password == '12345'){
-// jika nilai username dan password yang di inputkan user sama dengan nilai yang di tentukan
-// arahkan ke halaman dashboard
-// set session username
-    $_SESSION['username'] = $username;
-    header('Location: home.php');
-    exit();
-}else{
-// jika username dan password berbeda, redirect atau tetap di halaman index.php/login
-$_SESSION['error'] = "Username atau password salah!";
-header('Location: index.php'); // redicect kembali ke halaman login
-exit();
-}
-}
-?>
